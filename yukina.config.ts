71d879d55c775307cfe8c77f617e08561eb8cf0b
yngvi <yngvi161616@gmail.com>
2025-02-01 22:25:19 -0300

    banner

--- yukina.config.ts
+++ yukina.config.ts
@@ -45,10 +45,13 @@ const YukinaConfig: Configuration = {
   ],
 
   banners: [
-    "/images/banner3.jpg",
-    "/images/banner1.jpg",
-    "/images/banner4.jpg",
-    "/images/banner5.jpg",
+    "https://cdn.pixabay.com/photo/2015/06/14/23/35/rio-de-janeiro-809756_1280.jpg",
+    "https://cdn.pixabay.com/photo/2019/01/04/08/22/brasil-3912643_1280.jpg",
+    "https://cdn.pixabay.com/photo/2015/12/17/23/53/ouro-preto-1098064_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/01/15/23/46/rio-1142673_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/11/14/04/57/brazil-1822657_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2017/05/30/20/34/reflection-2358116_960_720.jpg",
+
 
     
   ],

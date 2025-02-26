855991c17791af754960d443a720af77dfa2771b
yngvi <yngvi161616@gmail.com>
2025-02-26 13:33:12 -0300

    ajuste commit

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,7 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { IdToSlug } from "../../utils/hash";
+import { IdToSlug } from '../utils/hash';
+
 
 export async function GET(context: { site: string }) {
   const posts = await getCollection("posts");

76a26ac9f2aaeb2a5dcd1a1698038a4e92f9b8e9
Yngvi01 <yngvi161616@gmail.com>
2025-04-12 15:49:25 -0300

    agora sim, nesse ponto toda parte visual esta funcional e mdx

--- src/components/MobileSearchBar.svelte
+++ src/components/MobileSearchBar.svelte
@@ -151,11 +151,15 @@
             resultPannel.style.height = newHeight;
           }
           resultPannel.style.opacity = "100%";
-          resultPannel.style.transform = "translateY(0)";
+          resultPannel.style.visibility = "visible";
+          resultPannel.style.marginTop = "8px";
+          resultPannel.style.pointerEvents = "auto";
         } else {
           resultPannel.style.height = "0px";
           resultPannel.style.opacity = "0";
-          resultPannel.style.transform = "translateY(-10px)";
+          resultPannel.style.visibility = "hidden";
+          resultPannel.style.marginTop = "0px";
+          resultPannel.style.pointerEvents = "none";
         }
       });
     };
@@ -186,6 +190,9 @@
           searchBar.style.opacity = "0";
           resultPannel.style.height = "0px";
           resultPannel.style.opacity = "0";
+          resultPannel.style.visibility = "hidden";
+          resultPannel.style.marginTop = "0px";
+          resultPannel.style.pointerEvents = "none";
         });
         
         searchBarDisplay = false;
@@ -218,6 +225,9 @@
         searchBar.style.opacity = "0";
         resultPannel.style.height = "0px";
         resultPannel.style.opacity = "0";
+        resultPannel.style.visibility = "hidden";
+        resultPannel.style.marginTop = "0px";
+        resultPannel.style.pointerEvents = "none";
         
         searchKeyword = "";
         searchResult = [];
@@ -250,11 +260,11 @@
 </div>
 
 <!-- mobile search bar -->
-<div class="fixed w-full z-20 top-[4.5rem] left-1/2 -translate-x-1/2">
+<div class="fixed w-full z-50 top-[4.5rem] left-0 right-0">
   <!-- search input pannel -->
   <div
     bind:this={searchBar}
-    class="absolute left-1/2 -translate-x-1/2 w-[95%] px-1 flex flex-col h-0 opacity-0 lg:hidden bg-[var(--card-color)] rounded-xl transition-all overflow-hidden before:content-[''] after:content-[''] before:pt-1 after:pb-1"
+    class="mx-auto w-[95%] px-1 flex flex-col h-0 opacity-0 lg:hidden bg-[var(--card-color)] rounded-xl transition-all overflow-hidden before:content-[''] after:content-[''] before:pt-1 after:pb-1"
   >
     <div class="bg-black/5 dark:bg-white/5 h-10 rounded-lg flex flex-row">
       <label
@@ -282,7 +292,7 @@
   <div
     id="result-pannel"
     bind:this={resultPannel}
-    class="max-h-[436px] overflow-y-scroll opacity-0 !absolute left-1/2 -translate-x-1/2 h-0 w-[95%] bg-[var(--card-color)] rounded-2xl top-[3.5rem] transition-all"
+    class="max-h-[436px] overflow-y-scroll opacity-0 mx-auto h-0 w-[95%] bg-[var(--card-color)] rounded-2xl mt-2 transition-all shadow-lg"
   >
     <div
       class="flex flex-col h-full onload-animation before:content-[''] before:pt-2 after:content-[''] after:pb-2"
